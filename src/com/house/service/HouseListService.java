242829cd13d65755e9b5c94efff5ac5b8d731aff
ehdtn3540 <ehdtn3540@gmail.com>
2021-01-25 17:59:15 +0900

    홈에서 리스트페이지로 이동 링크 걸었음

--- src/com/house/service/HouseListService.java
+++ src/com/house/service/HouseListService.java
@@ -9,6 +9,10 @@ public class HouseListService implements CommandAction{
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		request.setCharacterEncoding("UTF-8");
+		
+		String catg = request.getParameter("catg");
+		
 		
 		return "house/house_list.jsp";
 	}
