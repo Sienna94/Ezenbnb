6f1b5a16474f0de7ced6072004670cdf0b92c15f
ehdtn3540 <ehdtn3540@gmail.com>
2021-02-01 23:31:39 +0900

    서비스 추가

---
+++ src/com/house/service/HouseSearchService.java
@@ -0,0 +1,18 @@
+package com.house.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.CommandAction;
+
+public class HouseSearchService implements CommandAction {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		request.setCharacterEncoding("UTF-8");
+		
+		
+		return null;
+	}
+
+}
