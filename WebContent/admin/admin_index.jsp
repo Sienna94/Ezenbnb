298689bcd5ca16001e3d60b66e2c2fe8436160c6
ssol <haol0515@naver.com>
2021-02-04 11:37:12 +0900

    냥냥

--- WebContent/admin/admin_index.jsp
+++ WebContent/admin/admin_index.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@include file="../index.jsp" %>
+<%@include file="admin_menu.jsp" %>
 
 <h2>Welcome Admin</h2>
 
--- src/com/user/service/LoginService.java
+++ src/com/user/service/LoginService.java
@@ -36,7 +36,8 @@ public class LoginService implements CommandAction{
                 response.addCookie(cookie);
             }
             System.out.println("로그인 성공/ 아이디:" + userid);
-			return "main/main.jsp";
+			return "/index.jsp";
+		
 		}else {
 			session.setAttribute("logFail","아이디 혹은 패스워드가 잘못 입력되었습니다");
 			return "user/login/login.jsp";
